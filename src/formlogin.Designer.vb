6c0c78bbb3215125694b544cee78ab47c463098f
SerlyMutiaAnggraeni <serlymutiaanggraeni@gmail.com>
2026-01-07 22:18:31 +0700

    Add files via upload

---
+++ src/formlogin.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblJudul = New System.Windows.Forms.Label()
+        Me.lblUser = New System.Windows.Forms.Label()
+        Me.lblPass = New System.Windows.Forms.Label()
+        Me.txtUser = New System.Windows.Forms.TextBox()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.btnlogin = New System.Windows.Forms.Button()
+        Me.btncancel = New System.Windows.Forms.Button()
+        Me.cbshowpassword = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'lblJudul
+        '
+        Me.lblJudul.AutoSize = True
+        Me.lblJudul.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblJudul.Location = New System.Drawing.Point(368, 63)
+        Me.lblJudul.Name = "lblJudul"
+        Me.lblJudul.Size = New System.Drawing.Size(298, 38)
+        Me.lblJudul.TabIndex = 0
+        Me.lblJudul.Text = "LOGIN SISTEM KASIR"
+        '
+        'lblUser
+        '
+        Me.lblUser.AutoSize = True
+        Me.lblUser.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUser.Location = New System.Drawing.Point(171, 170)
+        Me.lblUser.Name = "lblUser"
+        Me.lblUser.Size = New System.Drawing.Size(133, 32)
+        Me.lblUser.TabIndex = 1
+        Me.lblUser.Text = "Username :"
+        '
+        'lblPass
+        '
+        Me.lblPass.AutoSize = True
+        Me.lblPass.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPass.Location = New System.Drawing.Point(181, 231)
+        Me.lblPass.Name = "lblPass"
+        Me.lblPass.Size = New System.Drawing.Size(123, 32)
+        Me.lblPass.TabIndex = 2
+        Me.lblPass.Text = "Password :"
+        '
+        'txtUser
+        '
+        Me.txtUser.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUser.Location = New System.Drawing.Point(328, 170)
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.Size = New System.Drawing.Size(347, 35)
+        Me.txtUser.TabIndex = 3
+        '
+        'txtPass
+        '
+        Me.txtPass.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPass.Location = New System.Drawing.Point(328, 231)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.Size = New System.Drawing.Size(347, 35)
+        Me.txtPass.TabIndex = 4
+        '
+        'btnlogin
+        '
+        Me.btnlogin.Location = New System.Drawing.Point(578, 351)
+        Me.btnlogin.Name = "btnlogin"
+        Me.btnlogin.Size = New System.Drawing.Size(97, 34)
+        Me.btnlogin.TabIndex = 5
+        Me.btnlogin.Text = "Login"
+        Me.btnlogin.UseVisualStyleBackColor = True
+        '
+        'btncancel
+        '
+        Me.btncancel.Location = New System.Drawing.Point(450, 351)
+        Me.btncancel.Name = "btncancel"
+        Me.btncancel.Size = New System.Drawing.Size(109, 34)
+        Me.btncancel.TabIndex = 6
+        Me.btncancel.Text = "Exit"
+        Me.btncancel.UseVisualStyleBackColor = True
+        '
+        'cbshowpassword
+        '
+        Me.cbshowpassword.AutoSize = True
+        Me.cbshowpassword.Location = New System.Drawing.Point(518, 285)
+        Me.cbshowpassword.Name = "cbshowpassword"
+        Me.cbshowpassword.Size = New System.Drawing.Size(144, 24)
+        Me.cbshowpassword.TabIndex = 7
+        Me.cbshowpassword.Text = "show password"
+        Me.cbshowpassword.UseVisualStyleBackColor = True
+        '
+        'FormLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1027, 578)
+        Me.Controls.Add(Me.cbshowpassword)
+        Me.Controls.Add(Me.btncancel)
+        Me.Controls.Add(Me.btnlogin)
+        Me.Controls.Add(Me.txtPass)
+        Me.Controls.Add(Me.txtUser)
+        Me.Controls.Add(Me.lblPass)
+        Me.Controls.Add(Me.lblUser)
+        Me.Controls.Add(Me.lblJudul)
+        Me.Name = "FormLogin"
+        Me.Text = "login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblJudul As Label
+    Friend WithEvents lblUser As Label
+    Friend WithEvents lblPass As Label
+    Friend WithEvents txtUser As TextBox
+    Friend WithEvents txtPass As TextBox
+    Friend WithEvents btnlogin As Button
+    Friend WithEvents btncancel As Button
+    Friend WithEvents cbshowpassword As CheckBox
+End Class
---
+++ src/formlogin.vb
@@ -0,0 +1,76 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class FormLogin
+
+
+    Private Sub FormLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtUser.Text = ""
+        txtPass.Text = ""
+
+        txtPass.PasswordChar = "*"
+
+        txtUser.Focus()
+    End Sub
+
+    Private Sub cbShowPassword_CheckedChanged(sender As Object, e As EventArgs) Handles cbshowpassword.CheckedChanged
+        If cbshowpassword.Checked = True Then
+
+            txtPass.PasswordChar = ControlChars.NullChar
+        Else
+
+            txtPass.PasswordChar = "*"
+        End If
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnlogin.Click
+
+
+        If txtUser.Text = "" Or txtPass.Text = "" Then
+            MsgBox("Username dan Password tidak boleh kosong!", MsgBoxStyle.Exclamation, "Peringatan")
+            Exit Sub
+        End If
+
+        Try
+            Call Koneksi()
+
+            Dim SQL As String = "SELECT * FROM user WHERE username='" & txtUser.Text & "' AND password='" & HitungMD5(txtPass.Text) & "'"
+
+            Cmd = New MySqlCommand(SQL, Conn)
+            Rd = Cmd.ExecuteReader
+            Rd.Read()
+
+            If Rd.HasRows Then
+
+
+                ModuleKoneksi.UserSedangLogin = Rd("nama_user")
+                ModuleKoneksi.LevelUser = Rd("role")
+
+                MsgBox("Login Berhasil! Selamat Datang, " & ModuleKoneksi.UserSedangLogin, MsgBoxStyle.Information, "Sukses")
+
+                Me.Hide()
+                FormUtama.Show()
+            Else
+
+                MsgBox("Username atau Password salah!", MsgBoxStyle.Critical, "Gagal Login")
+                txtPass.Text = ""
+                txtPass.Focus()
+            End If
+
+            Rd.Close()
+
+        Catch ex As Exception
+            MsgBox("Error Database: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btncancel.Click
+        End
+    End Sub
+
+    Private Sub txtPass_KeyDown(sender As Object, e As KeyEventArgs) Handles txtPass.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            btnLogin_Click(sender, e)
+        End If
+    End Sub
+
+End Class
